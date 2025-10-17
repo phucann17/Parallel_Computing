bc71c5d711b3f428c4fbdf8668a697125eb513b8
dxq2203 <quyet.dinhxuan@hcmut.edu.vn>
2025-10-17 17:40:32 +0700

    add strassen mpi parallel

--- Ass1/main.cpp
+++ Ass1/main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 // }
 
 // int main(int argc, char** argv) {
-//     int n[1] = {4000};
+//     int n[1] = {10000};
 //     for (int s = 0; s < 1; ++s) {
 //         cout << "Implementation with matrix size: " << n[s] << endl;
 //         matrix A = create_matrix(n[s]);
@@ -41,7 +41,7 @@ using namespace std;
 //         // // write_log("res.txt", res, n);  
 
 //         double start_1 = omp_get_wtime();
-//         parallel_matrix_multiplication_naive(A, B, res_naive_parallel, n[s]);
+//         // parallel_matrix_multiplication_naive(A, B, res_naive_parallel, n[s]);
 //         double end_1 = omp_get_wtime();
 
 //         printf("Time of parallel naive matrix multiplication algorithm = %.6f s\n", end_1- start_1);
@@ -67,21 +67,54 @@ using namespace std;
 //     }
 //     return 0;
 // }
+
+bool verify_matrix_multiplication(const matrix& A, const matrix& B, const matrix& res_test, unsigned int n) {
+    cout << "----------------------------------------" << endl;
+    cout << "Bat dau kiem tra tinh dung dan..." << endl;
+
+    // Tạo một ma trận để chứa kết quả đúng
+    matrix res_expected = create_matrix(n);
+
+    // Tính toán kết quả đúng bằng thuật toán tuần tự
+    cout << "Dang tinh toan ket qua chuan (sequential naive)..." << endl;
+    // sequential_matrix_multiplication_naive(A, B, res_expected, n);
+    sequential_matrix_multiplication_strassen(A, B, res_expected, n);
+    cout << "Da tinh toan xong ket qua chuan." << endl;
+
+    // So sánh từng phần tử
+    for (unsigned int i = 0; i < n; ++i) {
+        for (unsigned int j = 0; j < n; ++j) {
+            if (res_test[i][j] != res_expected[i][j]) {
+                cerr << "[LOI] Phat hien ket qua sai!" << endl;
+                cerr << "Tai vi tri [" << i << "][" << j << "]:" << endl;
+                cerr << "  - Ket qua tu MPI: " << res_test[i][j] << endl;
+                cerr << "  - Ket qua dung  : " << res_expected[i][j] << endl;
+                cout << "----------------------------------------" << endl;
+                return false; // Trả về false ngay khi tìm thấy lỗi
+            }
+        }
+    }
+
+    cout << "[OK] Ket qua hoan toan chinh xac!" << endl;
+    cout << "----------------------------------------" << endl;
+    return true; // Trả về true nếu tất cả các phần tử đều đúng
+}
+
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int n[1] = {4000};
+    int n[1] = {5000};
     for (int s = 0; s < 1; ++s) {
         if (rank == 0) {
             cout << "Implementation with matrix size: " << n[s] << endl;
         }
         matrix A = create_matrix(n[s]);
         matrix B = create_matrix(n[s]);
-        matrix res_naive_parallel = create_matrix(n[s]);
-        
+        // matrix res_naive_parallel = create_matrix(n[s]);
+        matrix res_strassen_parallel = create_matrix(n[s]);
         if (rank == 0) {
             for (int i = 0; i < n[s]; ++i){
                 for (int j = 0; j < n[s]; ++j){
@@ -91,18 +124,29 @@ int main(int argc, char** argv) {
             }
         }
 
-        // Broadcast matrix A to all processes
+        // Broadcast matrix A and B to all processes
         for (int i = 0; i < n[s]; ++i) {
             MPI_Bcast(A[i].data(), n[s], MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(B[i].data(), n[s], MPI_INT, 0, MPI_COMM_WORLD);
         }
 
+        // MPI_Barrier(MPI_COMM_WORLD); // Synchronize before start timing
+        // double start_2 = MPI_Wtime();
+        // mpi_parallel_matrix_multiplication_naive(A, B, res_naive_parallel, n[s]);
+        // MPI_Barrier(MPI_COMM_WORLD); // Ensure all processes have finished
+        // double end_2 = MPI_Wtime();
+        // if (rank == 0) {
+        //     printf("Time of mpi parallel naive matrix multiplication algorithm = %.6f s\n", end_2 - start_2);
+        //     verify_matrix_multiplication(A, B, res_naive_parallel, n[s]);
+        // }
         MPI_Barrier(MPI_COMM_WORLD); // Synchronize before start timing
-        double start_2 = MPI_Wtime();
-        mpi_parallel_matrix_multiplication_naive(A, B, res_naive_parallel, n[s]);
+        double start_4 = MPI_Wtime();
+        mpi_parallel_matrix_multiplication_strassen(A, B, res_strassen_parallel, n[s]);
         MPI_Barrier(MPI_COMM_WORLD); // Ensure all processes have finished
-        double end_2 = MPI_Wtime();
+        double end_4 = MPI_Wtime();
         if (rank == 0) {
-            printf("Time of mpi parallel naive matrix multiplication algorithm = %.6f s\n", end_2 - start_2);
+            printf("Time of mpi parallel strassen matrix multiplication algorithm = %.6f s\n", end_4 - start_4);
+            // verify_matrix_multiplication(A, B, res_strassen_parallel, n[s]);
         }
     }
 
