4388cff12684b279d1f4e07948535ff6462db9c8
dxq2203 <quyet.dinhxuan@hcmut.edu.vn>
2025-10-17 16:32:35 +0700

    add mpi parallel for native algorithm

--- Ass1/main.cpp
+++ Ass1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <omp.h>
+#include <mpi.h>
 #include "matrix_operations.h"
 #include "naive_mat_multiply_algo.h"
 #include "strassen_mat_multiply_algo.h"
@@ -13,51 +14,98 @@ using namespace std;
 //     free(m);
 // }
 
-int main(){
-    int n[1] = {2000};
+// int main(int argc, char** argv) {
+//     int n[1] = {4000};
+//     for (int s = 0; s < 1; ++s) {
+//         cout << "Implementation with matrix size: " << n[s] << endl;
+//         matrix A = create_matrix(n[s]);
+//         matrix B = create_matrix(n[s]);
+//         matrix res_naive_sequential = create_matrix(n[s]);
+//         matrix res_naive_parallel = create_matrix(n[s]);
+//         matrix res_strassen_sequential = create_matrix(n[s]);
+        
+//         for (int i = 0; i < n[s]; ++i){
+//             for (int j = 0; j < n[s]; ++j){
+//                 A[i][j] = (int)rand() / (16384*30);
+//                 B[i][j] = (int)rand() / (16384*30);
+//             }
+//         }
+
+//         double start = omp_get_wtime();
+//         // sequential_matrix_multiplication_naive(A, B, res_naive_sequential, n[s]);
+//         double end = omp_get_wtime();
+
+//         printf("Time of sequential naive matrix multiplication algorithm = %.6f s\n", end - start);
+//         // // write_log("A.txt", A, n);
+//         // // write_log("B.txt", B, n);
+//         // // write_log("res.txt", res, n);  
+
+//         double start_1 = omp_get_wtime();
+//         parallel_matrix_multiplication_naive(A, B, res_naive_parallel, n[s]);
+//         double end_1 = omp_get_wtime();
+
+//         printf("Time of parallel naive matrix multiplication algorithm = %.6f s\n", end_1- start_1);
+
+//         // double start_2 = omp_get_wtime();
+//         // mpi_parallel_matrix_multiplication_naive(A, B, res_naive_parallel, n[s]);
+//         // MPI_Barrier(MPI_COMM_WORLD);
+//         // double end_2 = omp_get_wtime();
+//         // printf("Time of mpi parallel naive matrix multiplication algorithm = %.6f s\n", end_2 - start_2);
+
+//         double start_3 = omp_get_wtime();
+//         cout << "hello !!!!" << endl;
+//         sequential_matrix_multiplication_strassen(A, B, res_strassen_sequential, n[s]);
+//         double end_3 = omp_get_wtime();
+
+//         printf("Time of sequential Strassen matrix multiplication algorithm = %.6f s\n", end_3 - start_3);
+
+//         // free_matrix(A, n[s]);
+//         // free_matrix(B, n[s]);
+//         // free_matrix(res_naive_sequential, n[s]);
+//         // free_matrix(res_naive_parallel, n[s]);
+//         // free_matrix(res_strassen_sequential, n[s]);
+//     }
+//     return 0;
+// }
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int n[1] = {4000};
     for (int s = 0; s < 1; ++s) {
-        cout << "Implementation with matrix size: " << n[s] << endl;
+        if (rank == 0) {
+            cout << "Implementation with matrix size: " << n[s] << endl;
+        }
         matrix A = create_matrix(n[s]);
         matrix B = create_matrix(n[s]);
-        matrix res_naive_sequential = create_matrix(n[s]);
         matrix res_naive_parallel = create_matrix(n[s]);
-        matrix res_strassen_sequential = create_matrix(n[s]);
         
-        for (int i = 0; i < n[s]; ++i){
-            for (int j = 0; j < n[s]; ++j){
-                A[i][j] = (int)rand() / (16384*30);
-                B[i][j] = (int)rand() / (16384*30);
+        if (rank == 0) {
+            for (int i = 0; i < n[s]; ++i){
+                for (int j = 0; j < n[s]; ++j){
+                    A[i][j] = (int)rand() / (16384*30);
+                    B[i][j] = (int)rand() / (16384*30);
+                }
             }
         }
 
-        double start = omp_get_wtime();
-        sequential_matrix_multiplication_naive(A, B, res_naive_sequential, n[s]);
-        double end = omp_get_wtime();
+        // Broadcast matrix A to all processes
+        for (int i = 0; i < n[s]; ++i) {
+            MPI_Bcast(A[i].data(), n[s], MPI_INT, 0, MPI_COMM_WORLD);
+        }
 
-        printf("Time of sequential naive matrix multiplication algorithm = %.6f s\n", end - start);
-        // // write_log("A.txt", A, n);
-        // // write_log("B.txt", B, n);
-        // // write_log("res.txt", res, n);  
-
-        double start_1 = omp_get_wtime();
-        parallel_matrix_multiplication_naive(A, B, res_naive_parallel, n[s]);
-        double end_1 = omp_get_wtime();
-
-        printf("Time of parallel naive matrix multiplication algorithm = %.6f s\n", end_1- start_1);
-
-        double start_3 = omp_get_wtime();
-        cout << "hello !!!!" << endl;
-        sequential_matrix_multiplication_strassen(A, B, res_strassen_sequential, n[s]);
-        double end_3 = omp_get_wtime();
-
-        printf("Time of sequential Strassen matrix multiplication algorithm = %.6f s\n", end_3 - start_3);
-
-        // free_matrix(A, n[s]);
-        // free_matrix(B, n[s]);
-        // free_matrix(res_naive_sequential, n[s]);
-        // free_matrix(res_naive_parallel, n[s]);
-        // free_matrix(res_strassen_sequential, n[s]);
+        MPI_Barrier(MPI_COMM_WORLD); // Synchronize before start timing
+        double start_2 = MPI_Wtime();
+        mpi_parallel_matrix_multiplication_naive(A, B, res_naive_parallel, n[s]);
+        MPI_Barrier(MPI_COMM_WORLD); // Ensure all processes have finished
+        double end_2 = MPI_Wtime();
+        if (rank == 0) {
+            printf("Time of mpi parallel naive matrix multiplication algorithm = %.6f s\n", end_2 - start_2);
+        }
     }
 
+    MPI_Finalize();
     return 0;
 }
--- Ass1/source/naive_mat_multiply_algo.cpp
+++ Ass1/source/naive_mat_multiply_algo.cpp
@@ -26,3 +26,125 @@ void parallel_matrix_multiplication_naive(const matrix& A, const matrix& B, matr
     }
 }
 
+// void mpi_parallel_matrix_multiplication_naive(const matrix& A, const matrix& B, matrix& res, unsigned int n) {
+//     int rank, size;
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+//     int rows_per_process = n / size;
+//     int start_row = rank * rows_per_process;
+//     int end_row = (rank == size - 1) ? n : start_row + rows_per_process;
+
+//     std::vector<int> Matrix_flat_B(n * n);
+//     if (rank == 0) {
+//         for (unsigned int i = 0; i < n; ++i) {
+//             for (unsigned int j = 0; j < n; ++j) {
+//                 Matrix_flat_B[i * n + j] = B[i][j];
+//             }
+//         }
+//     }
+
+//     // Broadcast matrix B to all processes
+//     MPI_Bcast(Matrix_flat_B.data(), n * n, MPI_INT, 0, MPI_COMM_WORLD);
+
+//     matrix local_B = create_matrix(n);
+//     for (unsigned int i = 0; i < n; ++i) {
+//         for (unsigned int j = 0; j < n; ++j) {
+//             local_B[i][j] = Matrix_flat_B[i * n + j];
+//         }
+//     }
+
+//     matrix local_res = create_matrix(end_row - start_row);
+//     for (int i = start_row; i < end_row; ++i) {
+//         for (unsigned int j = 0; j < n; ++j) {
+//             int tmp = 0;
+//             for (unsigned int k = 0; k < n; ++k) {
+//                 tmp += A[i][k] * local_B[k][j];
+//                 // tmp += A[i][k] * B[k][j];
+//             }
+//             local_res[i - start_row][j] = tmp;
+//         } 
+//     }
+
+//     if (rank == 0) {
+//         // Copy root process results
+//         for (int i = start_row; i < end_row; ++i) {
+//             res[i] = local_res[i - start_row];
+//         }
+//         // Collect results from all processes
+//         for (int p = 1; p < size; ++p) {
+//             int p_start_row = p * rows_per_process;
+//             int p_end_row = (p == size - 1) ? n - p_start_row : rows_per_process;
+//             for (int i = 0; i < p_end_row; ++i) {
+//                 MPI_Recv(res[p_start_row + i].data(), n, MPI_INT, p, p_start_row + i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//             }
+//         }
+//     } else {
+//         // Send local results to root process
+//         for (int i = 0; i < end_row - start_row; ++i) {
+//             MPI_Send(local_res[i].data(), n, MPI_INT, 0, start_row + i, MPI_COMM_WORLD);
+//         }
+//     }
+// }
+
+void mpi_parallel_matrix_multiplication_naive(const matrix& A, const matrix& B, matrix& res, unsigned int n) {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int rows_per_process = n / size;
+    int start_row = rank * rows_per_process;
+    int end_row = (rank == size - 1) ? n : start_row + rows_per_process;
+
+    std::vector<int> Matrix_flat_B(n * n);
+    if (rank == 0) {
+        for (unsigned int i = 0; i < n; ++i) {
+            for (unsigned int j = 0; j < n; ++j) {
+                Matrix_flat_B[i * n + j] = B[i][j];
+            }
+        }
+    }
+
+    // Broadcast matrix B to all processes
+    MPI_Bcast(Matrix_flat_B.data(), n * n, MPI_INT, 0, MPI_COMM_WORLD);
+
+    matrix local_B = create_matrix(n);
+    for (unsigned int i = 0; i < n; ++i) {
+        for (unsigned int j = 0; j < n; ++j) {
+            local_B[i][j] = Matrix_flat_B[i * n + j];
+        }
+    }
+
+    int num_rows_local = end_row - start_row;
+    matrix local_res(num_rows_local, std::vector<int>(n, 0));
+
+    for (int i = 0; i < num_rows_local; ++i) {
+        for (unsigned int j = 0; j < n; ++j) {
+            int tmp = 0;
+            for (unsigned int k = 0; k < n; ++k) {
+                tmp += A[start_row + i][k] * local_B[k][j];
+            }
+            local_res[i][j] = tmp;
+        }
+    }
+
+    if (rank == 0) {
+        // Copy root process results
+        for (int i = 0; i < num_rows_local; ++i) {
+            res[i] = local_res[i];
+        }
+        // Collect results from all other processes
+        for (int p = 1; p < size; ++p) {
+            int p_start_row = p * rows_per_process;
+            int p_rows = (p == size - 1) ? n - p_start_row : rows_per_process;
+            for (int i = 0; i < p_rows; ++i) {
+                MPI_Recv(res[p_start_row + i].data(), n, MPI_INT, p, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+        }
+    } else {
+        // Send local results to root process
+        for (int i = 0; i < num_rows_local; ++i) {
+            MPI_Send(local_res[i].data(), n, MPI_INT, 0, i, MPI_COMM_WORLD);
+        }
+    }
+}
