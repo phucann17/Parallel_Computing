091cb3891d52db04b8a0928b632c19dd512edcdd
phucann17 <annnguyen2210022@gmail.com>
2025-10-19 22:42:37 +0700

    update openMP strassen

--- Ass1/include/matrix_operations.h
+++ Ass1/include/matrix_operations.h
@@ -4,6 +4,7 @@
 typedef std::vector<std::vector<int>> matrix;
 
 matrix create_matrix(int n);
+matrix transpose(const matrix& B);
 void write_log(const char* filename, const matrix& m, unsigned int n);
 void sequential_matrix_addition(const matrix& a, const matrix& b, matrix& res, int n);
 void sequential_matrix_subtraction(const matrix& a, const matrix& b, matrix& res, int n);
