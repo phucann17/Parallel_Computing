75485daa94f27d2a8ca03f2c596a99695a6a355c
phucann17 <annnguyen2210022@gmail.com>
2025-12-11 19:12:16 +0700

    add size 10000

--- Ass1/main.cpp
+++ Ass1/main.cpp
@@ -117,7 +117,7 @@ void print_table(const vector<BenchmarkResult>& R) {
 }
 
 void run_all_benchmarks() {
-    vector<int> sizes = {100, 1000, 5000};
+    vector<int> sizes = {100, 1000, 5000, 10000};
     vector<BenchmarkResult> results;
 
     for (int n : sizes) {
